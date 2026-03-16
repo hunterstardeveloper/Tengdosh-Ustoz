7a07fd4bd1de2738b9e67b1d2a32ad493ab4ee4b
Hunterstar <kyuter789@gmail.com>
2026-03-17 03:00:00 +0500

    fixed

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const CACHE_VERSION = "tengdosh-v21.0.1";
+const CACHE_VERSION = "tengdosh-v21.0.2";
 const PRECACHE = `tengdosh-precache-${CACHE_VERSION}`;
 const RUNTIME = `tengdosh-runtime-${CACHE_VERSION}`;
 
